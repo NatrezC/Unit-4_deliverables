132fd6b7cd878c7e02c7e69c35969947e2685e5a
NatrezC <cnatrez@gmail.com>
2021-01-15 20:03:06 -0600

    created an array of letters to choose from

--- hang_man.py
+++ hang_man.py
@@ -15,25 +15,32 @@ import random #ask questions on importing
 
 word_list = ['batman', 'wonderwoman', 'thor', 'ironman', 'antman', 'hulk', 'elastigirl', 'humantorch', 'dash', 'blackpanther','megaman','wolverine','superman','elektra','daredevil','atom','aquaman','wasp','flash','staticshock','mrfantastic','robin']
 
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 # find a random word
 def word_finder(word_list):
     random_word = random.choice(word_list)
     return random_word
 
 #game_status
-def game_status():
+def game_status(abc):
     word = word_finder(word_list)
+    letters = set(abc)
     word_letter = set(word)
     user_tries =  6
+    used_letters = set()
+    # user_input = input('Choose your letter: ').lower()
+    print(letters)
+
+def user_letter_input(user_input):
+    if user_input == True:
+        pass
+    
+    
     
 
-def user_plays():
-    user_tries = 6
-    
-    
-
-
-user_status()
+game_status(abc)
+# user_letter_input(user_input)
 
 
 
